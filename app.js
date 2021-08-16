5133eb0be0fe49381d60d22a520c369cd761bfe7
Justin <justin.baptiste@gmail.com>
2021-08-16 15:53:59 -0400

    Update app.js
    This should make the images toggle

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function mtgImageDisplay() {
-    document.getElementById("uncleMtgImg1").style.display ='block';
-}
+    var x = document.getElementById("uncleMtgImg1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
