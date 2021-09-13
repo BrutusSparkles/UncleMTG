e43bcaf63b7a90a073a2f50a30dc33d59066b823
BrutusSparkles <61436533+BrutusSparkles@users.noreply.github.com>
2021-09-12 21:48:25 -0600

    Add files via upload

--- dice.js
+++ dice.js
@@ -2,13 +2,32 @@ var cube = document.querySelector('.cube');
 var rollBtn = document.querySelector('.rollBtn');
 var currentClass = '';
 
+//make the life totals disappear
+function hideLifeTotals() {
+  let lifeTotals = document.querySelectorAll('.lifeTotal');
+  for (let i = 0; i < lifeTotals.length; i++) {
+    lifeTotals[i].style.display = 'none';
+  }
+}
+
+//get a random number for roll dice funtion
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
+//make the life totals appear
+function showLifeTotals() {
+  let lifeTotals = document.querySelectorAll('.lifeTotal');
+  for (let i = 0; i < lifeTotals.length; i++) {
+    lifeTotals[i].style.display = 'block';
+  }
+}
+
+//this function handles the actual rolling of the dice
 function rollDice() {
+    hideLifeTotals();
     var randNum = getRandomInt(1,7);
     console.log(randNum);
     let showClass = 'show-' + randNum;
@@ -21,5 +40,4 @@ function rollDice() {
 }
 // set initial side
 rollDice();
-
-rollBtn.addEventListener("click", rollDice);
+showLifeTotals();
